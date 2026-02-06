6b227c0f85865576510d3be912f21738d8d0a45e
kythuatsonhungjsc-cmyk <kythuatsonhungjsc@gmail.com>
2026-02-06 10:56:31 +0700

    Add garage data to data.js

--- js/data.js
+++ js/data.js
@@ -14,6 +14,12 @@ export const DEFAULT_DATA = {
         { HoTen: "Hoàng Nam", ChucVu: "Pháp Chế", SDT: "", Nhom: "PHÁP CHẾ" },
         { HoTen: "Phòng Kế Toán", ChucVu: "Kế Toán", SDT: "", Nhom: "KẾ TOÁN" },
         { HoTen: "Phòng Nhân Sự", ChucVu: "Hành Chính", SDT: "", Nhom: "NHÂN SỰ" }
+    ],
+    gara: [
+        { GaRa: "Tuấn Anh", SoDienThoai: "84911795656", NghiepVu: "Điện gầm máy", DiaChi: "https://maps.app.goo.gl/Qi74xEg3tTjEwJ5QA" },
+        { GaRa: "Hoà Xồm", SoDienThoai: "84983081097", NghiepVu: "Gò hàn", DiaChi: "https://maps.app.goo.gl/oMDDJDRakjDw5g4w9" },
+        { GaRa: "Bạt Cô Tuyết", SoDienThoai: "84362470695", NghiepVu: "May vá bạt", DiaChi: "https://maps.app.goo.gl/xfu2CZUd84VqKqS39" },
+        { GaRa: "ISZ Quang Minh", SoDienThoai: "", NghiepVu: "Hãng ISZ", DiaChi: "https://maps.app.goo.gl/RyqftGtC7oV1ZnoM9" }
     ]
 };
 
